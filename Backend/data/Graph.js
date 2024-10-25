ad76d86397ce4d5970679cca014cba8311aa7e7c
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-10-25 11:06:34 +0300

    Created Graph class

---
+++ Backend/data/Graph.js
@@ -0,0 +1,26 @@
+const Node = require('Node.js')
+const Relation  = require('Require.js')
+
+class Graph {
+    constructor(){
+        this.nodes = []
+        this.relations = []
+    }
+
+    addNode(id, name, type){
+        const node = new Node(id, name, type)
+        this.nodes.push(node)
+    }
+
+    addRelation(from, to, type){
+        const from = this.nodes.find(node => node.id == from)
+        const to = this.nodes.find(node => node.id == to)
+        if(!from && !to){
+            throw new Error('Invalid node id!')
+        }
+        const relation = new Relation(from, to, type)
+        this.relations.push(relation)
+    }
+}
+
+module.exports = Graph
