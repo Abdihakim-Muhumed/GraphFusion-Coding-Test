0e62af5911df34ffed44a94af4a6111f120edd72
Abdihakim Muhumed <abdihakim.0017@gmail.com>
2024-10-26 07:18:54 +0300

    Created graph query methods

--- Backend/data/data.js
+++ Backend/data/data.js
@@ -2,4 +2,28 @@ const Graph = require('./Graph.js')
 
 const graphData = new Graph()
 
-module.exports = graphData
+
+const getGraphData = (req, res) => {
+    if(graphData.nodes.length == 0){
+        res.status(403).send("Graph data is empty! Add Nodes to fetch.")
+    }
+    res.status(200).json(graphData)
+}
+
+const addNode = (req, res) => {
+    const {id, name, type} = req.query
+    const addedNode = graphData.addNode(id, name, type)
+    res.status(201).json(addedNode)
+}
+
+const addRelation = (req, res) => {
+    const {from, to, relationship} = req.query
+    const addedRelation = graphData.addRelation(from, to, relationship)
+    res.status(201).json(addedRelation)
+}
+
+module.exports = {
+    getGraphData,
+    addNode,
+    addRelation
+}
